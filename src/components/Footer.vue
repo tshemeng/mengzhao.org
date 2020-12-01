b1729aa0b51d57cadeb1f151027483538b7ab1f5
mengzhao <mengzhao@clojurians.org>
2020-12-01 15:03:01 +0800

    init site

---
+++ src/components/Footer.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="text-xl mt-6">
+    <router-link class="icon-btn mx-2" to="/">
+      <Icon icon="carbon:campsite" class="inline-block" />
+    </router-link>
+
+    <div class="icon-btn mx-2" @click="isDark = !isDark">
+      <Icon :icon="isDark ? 'carbon:moon' : 'carbon:sun'" class="inline-block" />
+    </div>
+
+    <div class="icon-btn mx-2" @click="toggleLocales">
+      <Icon icon="carbon:language" class="inline-block" />
+    </div>
+
+    <router-link class="icon-btn mx-2" to="/about">
+      <Icon icon="carbon:dicom-overlay" class="inline-block" />
+    </router-link>
+
+    <a class="icon-btn mx-2" href="https://github.com/antfu/vitesse" target="_blank">
+      <Icon icon="carbon:logo-github" class="inline-block" />
+    </a>
+  </div>
+</template>
+
+<script setup lang='ts'>
+import { useI18n } from 'vue-i18n'
+import { locales } from '/~/messages'
+import { isDark } from '/~/logics'
+
+const i18n = useI18n()
+
+const toggleLocales = () => {
+  // change to some real logic
+  i18n.locale.value = locales[(locales.indexOf(i18n.locale.value) + 1) % locales.length]
+}
+</script>
---
+++ src/messages.ts
@@ -0,0 +1,9 @@
+import en from '../locales/en.json'
+import zhCN from '../locales/zh-CN.json'
+
+export const messages = {
+  'zh-CN': zhCN,
+  en,
+}
+
+export const locales = Object.keys(messages)
